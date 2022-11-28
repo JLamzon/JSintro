129c92e2e6c257dcbc9df565614983896aca72c0
JLamzon <jlamzon@yahoo.com>
2022-11-28 13:47:26 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+// alert("Welcome to JS")
+
+let firstName = 'Jessie';
+let numOne = 35;
+let truth = true;
+
+console.log(firstName, numOne, truth);
+console.log(numOne)
+console.log(truth);
+
+if (firstName == "doodoo")
+{
+    console.log("You got this!");
+}
+else
+{
+    console.log("Who are you?")
+}
+
+for (let i=0; i <= 100; i++)
+{
+    console.log(i);
+}
+
+let myArray = ["Mason", "Lincoln"]
+
+console.log(myArray);
+
+let arrayNum = [1, 20, 30, 40, 50, 80, 100];
+
+for(let i=0; i <= 40; i++)
+{
+    console.log()
+}
